8786673bd713e9f9a7e484d66b8cc521c6d804c7
Lucas Avila <lucas.avila@poatek.com>
2021-10-16 15:47:50 -0300

    Create email model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from dashboard.models import Email
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    fields = ('tittle', 'receivers', 'body')
+    list_display = ('tittle', 'status', 'receivers')
+    #TODO: button to send email
+    search_fields = ('tittle', 'receivers', 'body',)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Email(models.Model):
+
+    class EmailStatus(models.TextChoices):
+        PENDING = 'PE', _('Pending')
+        SENT = 'SE', _('Sent')
+        FAILED = 'FA', _('Failed')
+
+    id = models.BigAutoField(primary_key=True)
+    tittle = models.TextField(blank=False)
+    status = models.CharField(max_length=2, choices=EmailStatus.choices, default=EmailStatus.PENDING)
+    receivers = models.TextField(blank=False)
+    body = models.TextField(blank=False)
+
+    class Meta:
+        db_table = 'Email'
